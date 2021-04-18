88b9f0e66a39c80a6c70439e044537469d17e5d0
Екатерина Сочкова <volontare@yandex.ru>
2021-04-18 13:53:05 +0400

    Added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+    public void Delete(int ownerId, int postId) {
+        // TODO: add logic
+    }
+}
